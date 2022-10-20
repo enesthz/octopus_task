9e4ace32a895cf38a3bdbfdec1e1a4974c9c930c
Enes Taha Öz <enestahaoz@gmail.com>
2022-10-20 16:51:10 +0300

    create and develop window component

--- src/components/window.js
+++ src/components/window.js
@@ -1,7 +1,40 @@
-function Window(assetUrl){
+import { useEffect, useState } from "react"
+import assetsObject from './task.json'
+
+let assetsArray = Object.values(assetsObject);
+console.log(assetsArray)
+
+function Window(){
+    const [asset, setAsset] = useState(0);   
+     
+    let assetElement;
+
+    if(assetsArray[asset].type === "video"){
+        assetElement = <video src={assetsArray[asset].url} autoPlay muted></video>
+    }else{
+        assetElement = <img src = {assetsArray[asset].url}></img>
+    }
+
+    useEffect(() =>{
+        changeAsset();
+    });
+
+
+    function changeAsset(){
+        setTimeout(() => {
+            if(asset + 1 < assetsArray.length){
+                setAsset(asset + 1)
+            }else{
+                setAsset(0)
+            }
+        },assetsArray[asset].durationInSec * 1000);
+    }
+
     return(
         <div>
-            
+            {assetElement}
         </div>
     )
-}
+};
+
+export default Window
